7865019114c19bb51c700cf0bc77b4b1c1ac11e7
wangmh <810470114@qq.com>
2020-12-07 14:11:23 +0800

    样式初始化完成

--- web/.eslintrc.js
+++ web/.eslintrc.js
@@ -1,14 +1,25 @@
+/*
+ * @Author: your name
+ * @Date: 2020-11-24 17:21:42
+ * @LastEditTime: 2020-12-07 10:23:37
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \node-vue-demo\web\.eslintrc.js
+ */
 module.exports = {
   root: true,
   env: {
     node: true
   },
-  extends: ["plugin:vue/essential", "eslint:recommended", "@vue/prettier"],
+  extends: ['plugin:vue/essential', 'eslint:recommended', '@vue/prettier'],
   parserOptions: {
-    parser: "babel-eslint"
+    parser: 'babel-eslint'
   },
   rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off"
+    'no-unused-vars': 0,
+    // 关闭'prettier/prettier'的单双引号提示
+    'prettier/prettier': 'off',
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
   }
 };
--- web/src/main.js
+++ web/src/main.js
@@ -1,12 +1,22 @@
-import Vue from "vue";
-import App from "./App.vue";
-import router from "./router";
-import store from "./store";
+/*
+ * @Author: your name
+ * @Date: 2020-11-24 17:21:42
+ * @LastEditTime: 2020-12-07 10:13:07
+ * @LastEditors: your name
+ * @Description: In User Settings Edit
+ * @FilePath: \node-vue-demo\web\src\main.js
+ */
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router';
+import store from './store';
+
+import './assets/style.scss';
 
 Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
-  render: h => h(App)
-}).$mount("#app");
+  render: (h) => h(App)
+}).$mount('#app');
